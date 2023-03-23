5046f6df230d20161aaa301dad0cf5145691effc
Wenerson Venâncio <wenersonvenancio@MacBook-Pro-de-Wenerson-2.local>
2023-03-23 17:47:23 -0300

    CommitInicial

---
+++ lib/product.dart
@@ -0,0 +1,11 @@
+class Product {
+  final String imagePath;
+
+  final String title;
+
+  final int cost;
+
+  final int reviewCount;
+
+  Product(this.imagePath, this.title, this.cost, this.reviewCount);
+}
